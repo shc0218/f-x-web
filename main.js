372a8587a4825b7d9ccb698b7117bd59482ddd52
shc0218 <54468708+shc0218@users.noreply.github.com>
2022-05-31 00:56:16 +0900

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+let x;
+function f(x) {
+    if (x >= 0 && x <= 3) {
+      return x - 1
+    } else if (x > 3) {
+       return  f(x - 3);
+    } else {
+        return "0이상값 입력하라고 병신아"
+    }
+}
+function onSubmit() {
+    console.log(f(x) + "    "  + x);
+    document.getElementById("result").innerText = f(x);
+}
+
+function onChangeTextBox(obj) {
+    x = obj.value;
+}
