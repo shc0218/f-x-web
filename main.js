4849211b06260d9eeb62155ddc6f4446709ec2c4
shc0218 <54468708+shc0218@users.noreply.github.com>
2022-05-31 01:14:59 +0900

    Update main.js

--- main.js
+++ main.js
@@ -4,6 +4,8 @@ function f(x) {
       return x - 1
     } else if (x > 3) {
        return  f(x - 3);
+    } else if (x =="최예원 바보") }
+         return  "맞지"
     } else {
         return "0이상값 입력하라고 병신아"
     }
